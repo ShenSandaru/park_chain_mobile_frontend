820e64f465eb75cc3e57816e76c499db35d96065
ShenSandaru <136662438+ShenSandaru@users.noreply.github.com>
2025-12-03 23:11:47 +0530

    initialized the project structure and start to begin the coding and the creating the code screen files

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,5 @@
 //This is Main App Widget (Routing & Theme setup)
+
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
